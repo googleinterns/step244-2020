aa29fec86d6bec1726f3f384e9e98b11760d9a0f
Sofia <sofia.ladypython@gmail.com>
2020-08-24 16:52:39 +0000

    Refactor skeleton

--- src/main/java/com/google/sps/servlets/JoinServlet.java
+++ src/main/java/com/google/sps/servlets/JoinServlet.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/events/join")
+@WebServlet("/join")
 public class JoinServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
