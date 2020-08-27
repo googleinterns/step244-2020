589fb89ce86e72691b8e65ee8ca652ee5a329bf0
Andru Stefanescu <47646359+andru47@users.noreply.github.com>
2020-08-27 20:53:47 +0300

    Gcalendar branch (#9)
    * Backend for GCalendar Event getting
    
    * Frontend for calendar interface
    
    * Solve comments
    
    * Solve comments 2
    
    * Solve comments 3

--- src/main/java/com/google/sps/servlets/EventServlet.java
+++ src/main/java/com/google/sps/servlets/EventServlet.java
@@ -14,6 +14,10 @@
 
 package com.google.sps.servlets;
 
+import com.google.api.client.http.HttpStatusCodes;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event.ExtendedProperties;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
@@ -22,7 +26,12 @@ import com.google.sps.data.EventStorage;
 import com.google.sps.data.User;
 import com.google.sps.data.UserStorage;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import com.google.api.client.http.HttpHeaders;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +40,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/events/*")
 public class EventServlet extends HttpServlet {
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String pathName = request.getPathInfo();
+    UserService userService = UserServiceFactory.getUserService();
+
+    if (pathName.equals("/gcalendar")) {
+      getEvents(request, response, userService);
+      return;
+    }
     // TODO: parse "events/{event_id}" here.
     response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
   }
@@ -57,5 +73,63 @@ public class EventServlet extends HttpServlet {
 
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
+  }
+
+  private void getEvents(HttpServletRequest request, HttpServletResponse response, UserService userService)
+      throws IOException {
+    String startEpochInSeconds = request.getParameter("startEpochInSeconds");
+    String endEpochInSeconds = request.getParameter("endEpochInSeconds");
+
+    if (startEpochInSeconds == null || endEpochInSeconds == null || !startEpochInSeconds.matches("^[0-9]+$")
+        || !endEpochInSeconds.matches("^[0-9]+$")) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+
+    if (!userService.isUserLoggedIn()) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      return;
+    }
+    Long startEpochInSecondsParsed = parseLongFromString(startEpochInSeconds);
+    Long endEpochInSecondsParsed = parseLongFromString(endEpochInSeconds);
+
+    if (startEpochInSecondsParsed < 0 || endEpochInSecondsParsed < 0
+        || startEpochInSecondsParsed > endEpochInSecondsParsed) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+
+    String eventsInJson = getGCalendarEventsByInterval(startEpochInSecondsParsed, endEpochInSecondsParsed);
+    if (eventsInJson == null)
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    else
+      response.getWriter().println(eventsInJson);
+    return;
+  }
+
+  private long parseLongFromString(String str) {
+    try {
+      long nr = Long.parseLong(str);
+      return nr;
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
+  private String getGCalendarEventsByInterval(Long startEpochInSeconds, Long endEpochInSeconds) throws IOException {
+    DateTime startDateTime = new DateTime(startEpochInSeconds);
+    DateTime endDateTime = new DateTime(endEpochInSeconds);
+    try {
+      Calendar service = Utils.loadCalendarClient();
+      HttpHeaders headers = new com.google.api.client.http.HttpHeaders()
+          .setAcceptEncoding("gzip").setUserAgent("gzip");
+      List<com.google.api.services.calendar.model.Event> events = service.events().list("primary")
+          .setFields("items(summary,start,end,description,extendedProperties,location)").setSingleEvents(true)
+          .setTimeMin(startDateTime).setTimeMax(endDateTime).setRequestHeaders(headers).execute().getItems();
+      return new Gson().toJson(events);
+    } catch (GeneralSecurityException e) {
+      e.printStackTrace();
+    }
+    return null;
   }
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -28,15 +28,16 @@ function searchEvents() {
 
 function joinEvent(event_id) {
   fetch('/events/' + event_id + '/join').then(response => response.json());
+}
 
 function addEventToGCalendar() { //To be modified to get fields
   var resp = verifyCredentials().then(validCredential => {
     if (validCredential == true) {
       fetch("/events/gcalendar", { method: "POST" }).then(response => {
-        window.location.href = "http://localhost:8080/calendar.html";
+        window.location.href = getCurrentUrl() + "/calendar.html";
       }).catch(error => alert(error));
     } else {
-      window.location.href = "http://localhost:8080/token?origin=calendar"; //To be modified to get current location
+      window.location.href = getCurrentUrl() + "/token?origin=calendar";
     }
   });
 }
@@ -48,4 +49,164 @@ function verifyCredentials() {
     alert(error);
     return false;
   });
+}
+
+function getGCalendarEvents(calendar, startTime, endTime) {
+  verifyCredentials().then(validCredential => {
+    if (validCredential) {
+      fetch("/events/gcalendar?startEpochInSeconds=" + startTime + "&endEpochInSeconds=" + endTime)
+        .then(response => response.json()).then(events => {
+          var fullcalendarEvents = [];
+          events.forEach(event => {
+            var start, end;
+            if (event.start.dateTime) {
+              start = event.start.dateTime.value;
+              end = event.end.dateTime.value;
+            } else {
+              start = event.start.date.value;
+              end = event.end.date.value;
+            }
+            var shared = null, private = null;
+            if (event.extendedProperties)
+              shared = event.extendedProperties.shared, private = event.extendedProperties.private;
+            fullcalendarEvents.push({
+              id: event.id,
+              title: event.summary,
+              start: start,
+              end: end,
+              location: event.location,
+              description: event.description,
+              shared: shared,
+              private: private
+            });
+          });
+          if (calendar.getEventSources().length)
+            calendar.getEventSources()[0].remove();
+          calendar.addEventSource(fullcalendarEvents);
+        });
+    } else {
+      window.location.href = getCurrentUrl() + "/token?origin=calendar";
+    }
+  });
+}
+
+function getCurrentUrl(){
+  var currentUrl = window.location.href;
+  var currentUrlSlices = currentUrl.split("/");
+  return currentUrlSlices[0] + "//" + currentUrlSlices[2];
+}
+
+function createCalendarElements(givenProperties) {
+  var eventWrapper = document.createElement("div");
+  eventWrapper.classList.add("event-wrapper");
+
+  var contentWrapper = document.createElement("div");
+  contentWrapper.classList.add("content-wrapper");
+
+  var descriptionElement = document.createElement("div");
+  descriptionElement.classList.add("description-wrapper");
+  if (givenProperties.description) {
+    var descriptionContent = document.createElement("div");
+    descriptionContent.innerHTML = givenProperties.description;
+    descriptionContent.classList.add("right-item");
+    var descriptionIcon = document.createElement("i");
+    descriptionIcon.classList.add("fa", "fa-bars", "left-item");
+    descriptionElement.appendChild(descriptionIcon);
+    descriptionElement.appendChild(descriptionContent);
+  }
+
+  var locationElement = document.createElement("div");
+  locationElement.classList.add("extendedprop-wrapper");
+  if (givenProperties.location) {
+    var locationIcon = document.createElement("i");
+    locationIcon.classList.add("fa", "fa-location-arrow", "left-item");
+    var locationContent = document.createElement("div");
+    locationContent.innerHTML = givenProperties.location;
+    locationContent.classList.add("right-item");
+    locationElement.appendChild(locationIcon);
+    locationElement.appendChild(locationContent);
+  }
+
+  eventWrapper.appendChild(descriptionElement);
+  eventWrapper.appendChild(locationElement);
+
+  if (givenProperties.shared) {
+    for (const [key, value] of Object.entries(givenProperties.shared)) {
+      var extendedPropertyElement = document.createElement("div");
+      extendedPropertyElement.classList.add("extendedprop-wrapper");
+      var extendedPropertyName = document.createElement("div");
+      extendedPropertyName.innerHTML = key;
+      extendedPropertyName.classList.add("left-item");
+      var extendedPropertyValue = document.createElement("div");
+      extendedPropertyValue.innerHTML = value;
+      extendedPropertyValue.classList.add("right-item");
+      extendedPropertyElement.appendChild(extendedPropertyName);
+      extendedPropertyElement.appendChild(extendedPropertyValue);
+      eventWrapper.appendChild(extendedPropertyElement);
+    }
+  }
+
+  var buttonDivElement = document.createElement("div");
+  buttonDivElement.classList.add("add-button");
+  var buttonElement = document.createElement("button");
+  buttonElement.classList.add("btn", "btn-primary");
+  var plusIcon = document.createElement("i");
+  plusIcon.classList.add("fas", "fa-edit");
+
+  buttonElement.appendChild(plusIcon);
+  buttonDivElement.appendChild(buttonElement);
+  eventWrapper.appendChild(buttonDivElement);
+  return eventWrapper;
+}
+
+function hasAnyParentWithGivenId(parent, id) {
+  while (parent != null && parent.nodeName != "BODY") {
+    if (parent.id === id)
+      return true;
+    parent = parent.parentElement;
+  }
+  return false;
+}
+
+function isIdValid(givenId) {
+  return givenId.length && document.getElementById(givenId);
+}
+
+function calendarRender() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'timeGridWeek',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'timeGridDay,timeGridWeek,dayGridMonth'
+    },
+    eventDidMount: function (info) {
+      $(info.el).popover({
+        animation: true,
+        html: true,
+        title: info.event.title,
+        content: createCalendarElements(info.event.extendedProps),
+        trigger: 'click',
+        container: 'body',
+        placement: 'top',
+      });
+    },
+    datesSet: function (info) {
+      var viewUTCStartTime = info.view.activeStart.getTime() - info.view.activeStart.getTimezoneOffset();
+      var viewUTCEndTime = info.view.activeEnd.getTime() - info.view.activeEnd.getTimezoneOffset();
+      getGCalendarEvents(calendar, viewUTCStartTime, viewUTCEndTime);
+    },
+    fixedWeekCount: false,
+    eventClick: function (event) {
+      event.jsEvent.preventDefault();
+    },
+    eventTimeFormat: {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    },
+    displayEventEnd: true
+  });
+  calendar.render();
 }
