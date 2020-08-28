1f68de4ccf0cc0d541f7e81e46bb5b51212691ac
Sofia <sofia.ladypython@gmail.com>
2020-08-28 10:15:15 +0000

    Fix style errors

--- src/main/java/com/google/sps/data/Event.java
+++ src/main/java/com/google/sps/data/Event.java
@@ -16,7 +16,6 @@ package com.google.sps.data;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
