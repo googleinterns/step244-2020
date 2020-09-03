6b010ef0828f083d4eb3b963a0f8c6dc1f143df9
Sofia <sofia.ladypython@gmail.com>
2020-09-03 12:18:19 +0000

    Chack for null

--- src/main/java/com/google/sps/servlets/EventServlet.java
+++ src/main/java/com/google/sps/servlets/EventServlet.java
@@ -215,6 +215,14 @@ public class EventServlet extends HttpServlet {
       UserStorage.joinEvent(currentUserId, eventId);
       User user = UserStorage.getUser(currentUserId);
       Event event = EventStorage.getEvent(eventId);
+      if (user == null) {
+        System.err.println("Can't find user with id " + currentUserId);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      }
+      if (event == null) {
+        System.err.println("Can't find event with id " + eventId);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      }
       Utils.joinGCalendarEvent(event.getOwnerID(), eventId, user.getEmail());
     } catch (Exception e) {
       // TODO: specify exception
