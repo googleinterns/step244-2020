a16acc1995a38ec30d11745554e45eb2b85ee640
Andru Stefanescu <andru.stefanescu@gmail.com>
2020-09-08 14:06:04 +0300

    Change servlets to handle user info

--- src/main/java/com/google/sps/servlets/AuthServlet.java
+++ src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -1,6 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +13,8 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import com.google.sps.data.AuthData;
+import com.google.sps.data.User;
+import com.google.sps.data.UserStorage;
 
 @WebServlet("/auth")
 public class AuthServlet extends HttpServlet {
@@ -19,6 +22,14 @@ public class AuthServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String origin = request.getParameter("origin");
     UserService userService = UserServiceFactory.getUserService();
+    if (userService.isUserLoggedIn() && UserStorage.getUser(userService.getCurrentUser().getUserId()) == null) {
+      UserStorage.addOrUpdateUser(new User(userService.getCurrentUser().getUserId(),
+                                           userService.getCurrentUser().getEmail(),
+                                           null,
+                                           new ArrayList<>(),
+                                           new ArrayList<>(),
+                                           new ArrayList<>()));
+    }
     response.setContentType("application/json;");
     Gson gson = new Gson();
     response.getWriter().println(gson.toJson(new AuthData(userService.isUserLoggedIn(), buildAuthLink(userService, origin))));
